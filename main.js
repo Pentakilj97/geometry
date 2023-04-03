cfa7f26a0441404845f80c2006ff180a978753b0
crex02 <127298119+Pentakilj97@users.noreply.github.com>
2023-04-03 13:34:39 +0200

    up

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const point1= new Point(1,1);
+const point2= new Point(4,5);
+
+console.log(point1);
+console.log(point2);
+
+const segment1= new Segment(point1, point2);
+
+console.log(segment1.calculateLength());
+
+const segment2=Segment.fromPointsCoordinates(0,1,3,5);
+
+console.log(segment2)
+
